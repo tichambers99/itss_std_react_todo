fa110197d938187e14f7a5db9c8398fb156d4a0c
EC2 Default User <ec2-user@ip-172-31-7-141.ec2.internal>
2021-05-21 09:06:53 +0000

    use firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD-CWqnBrYXpdk0syKyOXC4YEb76h6z4D8",
+    authDomain: "jp3-todolist-react.firebaseapp.com",
+    projectId: "jp3-todolist-react",
+    storageBucket: "jp3-todolist-react.appspot.com",
+    messagingSenderId: "12475153200",
+    appId: "1:12475153200:web:11e83e1b60f2c8681466f5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
