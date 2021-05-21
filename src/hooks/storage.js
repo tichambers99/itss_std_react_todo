002c2a82fb233d947b96891c8885fc39adde4420
EC2 Default User <ec2-user@ip-172-31-7-141.ec2.internal>
2021-05-21 10:22:19 +0000

    add, update, clear item

--- src/hooks/storage.js
+++ src/hooks/storage.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import db from '../lib/firebase.js';
 
-const STORAGE_KEY = 'itss-todo';
+const STORAGE_KEY = 'todos';
 
 function useStorage() {
   const [items, setItems] = useState([]);
@@ -9,9 +9,7 @@ function useStorage() {
   useEffect(() => {
     //const data = localStorage.getItem(STORAGE_KEY);
     
-    const data = [];
-    
-    db.collection("todos").onSnapshot(snapshot=>{
+    db.collection(STORAGE_KEY).onSnapshot(snapshot=>{
       setItems(snapshot.docs.map(doc=>({
         key: doc.id,
         text: doc.data().text,
@@ -25,18 +23,33 @@ function useStorage() {
     // }
   }, []);
 
-  const putItems = items => {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(items));
-    setItems  (items);
+  const addItems = async item => {
+    await db.collection(STORAGE_KEY).add({
+      text: item,
+      done: false,
+    })
+    setItems(items);
+  };
+  
+  const updateItems = async item => {
+    const updateItems = db.collection(STORAGE_KEY).doc(item.key);
+    await updateItems.update({
+      done: !item.done,
+    })
   };
 
-  const clearItems = () => {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify([]));
-    setItems([]);
+  const clearItems =async () => {
+    const allItems = await db.collection(STORAGE_KEY).get();
+    const batch = db.batch();
+    allItems.forEach(item=>{
+      batch.delete(item.ref);
+    })
+    
+    await batch.commit();
     
   };
 
-  return [items, putItems, clearItems];
+  return [items, addItems, updateItems, clearItems];
 }
 
 export default useStorage;
